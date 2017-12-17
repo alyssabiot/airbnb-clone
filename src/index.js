652a6e237d8bea5969d5123d4df0cd39800a1ab2
Alyssa Biot <alyssa.biot@gmail.com>
2017-12-17 15:14:08 +0100

    create grid for flats and map

--- src/index.js
+++ src/index.js
@@ -1,11 +1,11 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
-// import App from './App';
-import Hello from './components/hello';
+import App from './App';
+// import Hello from './components/hello';
 // import registerServiceWorker from './registerServiceWorker';
 
-const root = <Hello firstName="Alyssa" lastName="Biot" />;
+// const root = <Hello firstName="Alyssa" lastName="Biot" />;
 
-ReactDOM.render(root, document.getElementById('root'));
+ReactDOM.render(<App />, document.getElementById('root'));
 // registerServiceWorker();
